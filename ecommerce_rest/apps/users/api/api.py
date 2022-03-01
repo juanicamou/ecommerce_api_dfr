62aa5493d6acaa9cef76bd24129c9942e568b9b0
Juan Ignacio Camou <juanignaciocamou@gmail.com>
2022-03-01 00:37:29 -0300

    Adds update and delete methods

--- ecommerce_rest/apps/users/api/api.py
+++ ecommerce_rest/apps/users/api/api.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
@@ -6,24 +7,42 @@ from apps.users.api.serializers import UserSerializer
 
 @api_view(['GET'])
 def user_get_all_api_view(request): # Returns all users
-    if request.method == 'GET': 
-        users = User.objects.all() # Brings all objects
-        users_serializer = UserSerializer(users, many = True) # Tranforms model object in json (Serializer task)
-        return Response(users_serializer.data) # Returns the json response
+    users = User.objects.all() # Brings all objects
+    users_serializer = UserSerializer(users, many = True) # Tranforms model object in json (Serializer task)
+    return Response(users_serializer.data, status = status.HTTP_200_OK) # Returns the json response
 
 @api_view(['POST'])
 def user_create_api_view(request): # Creates a new user
-    if request.method == 'POST': 
-        user_serializer = UserSerializer(data = request.data) # Tranforms json in model object (Serializer task)
-        if user_serializer.is_valid(): # Checks if the request data is valid with model 
-            user_serializer.save() # Saves the object
-            return Response(user_serializer.data) # Returns a json response with creation information
-        return Response(user_serializer.errors) # Returns a json response with the error information
+    user_serializer = UserSerializer(data = request.data) # Tranforms json in model object (Serializer task)
+    if user_serializer.is_valid(): # Checks if the request data is valid with model 
+        user_serializer.save() # Saves the object
+        return Response(user_serializer.data, status = status.HTTP_201_CREATED) # Returns a json response with creation information
+    return Response(user_serializer.errors, status = status.HTTP_400_BAD_REQUEST) # Returns a json response with the error information
 
 
 @api_view(['GET'])
-def user_detail_api_view(request, pk):
-    if request.method == 'GET':
-        user = User.objects.filter(id = pk).first()
-        user_serializer = UserSerializer(user)
-        return Response(user_serializer.data)
+def user_detail_api_view(request, pk): # Shows the detail of a user
+    user = User.objects.filter(id = pk).first() # Brings the object
+    if user:
+        user_serializer = UserSerializer(user) # Tranforms model object in json (Serializer task)
+        return Response(user_serializer.data, status = status.HTTP_200_OK) # Returns the json response
+    return Response({'message': 'A user with the input data has not been found.'}, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def user_update_api_view(request, pk): # Updates an user
+    user = User.objects.filter(id = pk).first() # Brings the object
+    if user:
+        user_serializer = UserSerializer(user, data = request.data) # Tranforms json in model object (Serializer task)
+        if user_serializer.is_valid(): # Checks if the request data is valid with model 
+            user_serializer.save() # Saves the object
+            return Response(user_serializer.data, status = status.HTTP_200_OK) # Returns a json response with modification information
+        return Response(user_serializer.errors, status = status.HTTP_400_BAD_REQUEST)  # Returns a json response with the error information
+    return Response({'message': 'A user with the input data has not been found.'}, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def user_delete_api_view(request, pk): # Deletes an user
+    user = User.objects.filter(id = pk).first() # Brings the object
+    if user:
+        user.delete() # Deletes the user
+        return Response({'message': 'The user was deleted successfully'}, status = status.HTTP_200_OK)
+    return Response({'message': 'A user with the input data has not been found.'}, status = status.HTTP_400_BAD_REQUEST)
--- ecommerce_rest/apps/users/api/urls.py
+++ ecommerce_rest/apps/users/api/urls.py
@@ -1,9 +1,11 @@
 from unicodedata import name
 from django.urls import path
-from apps.users.api.api import user_get_all_api_view, user_create_api_view, user_detail_api_view
+from apps.users.api.api import user_get_all_api_view, user_create_api_view, user_detail_api_view, user_update_api_view, user_delete_api_view 
 
 urlpatterns = [
     path('all/', user_get_all_api_view, name = 'user_all'),
     path('create/', user_create_api_view, name = 'user_create'),
-    path('detail/<int:pk>/', user_detail_api_view, name = 'user_details')
+    path('detail/<int:pk>/', user_detail_api_view, name = 'user_details'),
+    path('update/<int:pk>/', user_update_api_view, name = 'user_update'),
+    path('delete/<int:pk>/', user_delete_api_view, name = 'user_delete')
 ]
